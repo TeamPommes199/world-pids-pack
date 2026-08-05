09799433f50cd558be17d6de792111ce7c02a21c
TeamPommes199 <luisschmidt2010@gmail.com>
2026-08-05 22:52:35 +0200

    - add Hamburg (S-Bahn) to the pids_1a

--- assets/jsblock/scripts/hamburg_sbahn.js
+++ assets/jsblock/scripts/hamburg_sbahn.js
@@ -189,8 +189,13 @@ function render(ctx, state, pids) {
         .draw(ctx);
     }
 
+    let now = new Date();
+    let hours = now.getHours().toString().padStart(2, '0');
+    let minutes = now.getMinutes().toString().padStart(2, '0');
+    let time = hours + ":" + minutes + " Uhr"
+
     Text.create("Clock")
-    .text(PIDSUtil.formatTime(MinecraftClient.worldDayTime(), true) + " Uhr")
+    .text(time)
     .color(0xFFFFFF)
     .pos(pids.width - 2.5, 2)
     .scale(0.4)
